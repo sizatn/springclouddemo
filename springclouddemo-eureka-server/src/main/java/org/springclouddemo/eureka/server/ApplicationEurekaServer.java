844b2a71d74508c1605bd827cf161691add165ab
sizatn <sizatn2013@gmail.com>
2019-08-25 14:19:51 +0800

    first commit

---
+++ springclouddemo-eureka-server/src/main/java/org/springclouddemo/eureka/server/ApplicationEurekaServer.java
@@ -0,0 +1,15 @@
+package org.springclouddemo.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ApplicationEurekaServer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationEurekaServer.class, args);
+	}
+	
+}
